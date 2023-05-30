16f05ca65b6569984b00ce8bde39e29d7e67d740
Ken Atopos <z1158229761@outlook.com>
2023-05-30 22:16:49 +0800

    finish state learning

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,17 +1,28 @@
+import "./App.css";
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
-  return (
-    <div>
-      <button onClick={handleClick}>Add animal</button>
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
 
-      <div>Number of animals are {count}</div>
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
