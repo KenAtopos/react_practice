a5a7249c991260e73088d7b7d781bc33d6d6487b
Ken Atopos <z1158229761@outlook.com>
2023-05-30 20:25:57 +0800

    start to use state

---
+++ animals/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add animal</button>
+
+      <div>Number of animals are {count}</div>
+    </div>
+  );
+}
+
+export default App;
